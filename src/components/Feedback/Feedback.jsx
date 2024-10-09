4346e6a1f066ce6581fff839749f9947f3570ceb
Gustavo Portela <126726122+portelagu@users.noreply.github.com>
2024-10-09 19:53:56 +0000

    finish Feedback component

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,11 +1,44 @@
 import './Feedback.css'
+import FeedbackContainer from './FeedbackContainer/FeedbackContainer';
+import icon1 from '../../assets/icons/user-icon-1.svg'
+import icon2 from '../../assets/icons/user-icon-2.svg'
+import icon3 from '../../assets/icons/user-icon-3.svg'
+import icon4 from '../../assets/icons/user-icon-4.svg'
 
-function Feedback(){
-    return(
+function Feedback() {
+    return (
         <div>
-<section className="feedback-section">
-    <p>Teste</p>
-</section>
+            <div className='yellow-line'></div>
+                <div className="feedback-title-subtitle">
+                    <h1 className="feedback-title">Feedback dos Nossos Clientes</h1>
+                    <p className="feedback-subtitle">Veja como cuidamos com carinho e dedicação dos pets de quem confia na PetLiving</p>
+                </div>
+
+                <section className="feedback-section">
+                <FeedbackContainer
+                    icon={icon1}
+                    name="Luana Queiroz"
+                    date="Cliente a 2 meses"
+                    text='"Eu simplesmente amo a PetLiving! O atendimento foi excepcional desde o primeiro contato, e meu cachorro, Max, voltou para casa tão feliz e cheirosinho após o banho e tosa. A equipe realmente demonstra carinho e cuidado com os pets, tratando cada um como se fosse da família."' />
+
+                <FeedbackContainer
+                    icon={icon2}
+                    name="João Pereira"
+                    date="Cliente a 1 ano e 5 meses"
+                    text='"A PetLiving é simplesmente maravilhosa! Levei meu gato, Felix, para vacinação e fiquei impressionado com a atenção e carinho da equipe. Eles explicaram todo o processo e fizeram com que eu me sentisse tranquilo. É ótimo saber que meu pet está em boas mãos. Recomendo de olhos fechados!"' />
+
+                <FeedbackContainer
+                    icon={icon3}
+                    name="Karina Costa"
+                    date="Cliente a 9 meses"
+                    text='"Simplesmente amei a experiência com a PetLiving! Deixei meu cachorro, Buddy, para um dia de cuidados e ele voltou tão feliz! A equipe foi extremamente atenciosa e me enviou atualizações ao longo do dia. É ótimo saber que meu pet está seguro e bem tratado. Vou sempre escolher a PetLiving para os cuidados do meu Buddy!"' />
+
+                <FeedbackContainer
+                    icon={icon4}
+                    name="Clara Aguiar"
+                    date="Cliente a 2 anos"
+                    text='"Experiência incrível com a PetLiving! Deixei meu gato, Simba, para um check-up e a equipe foi fantástica. Eles foram muito cuidadosos e atenciosos, explicando cada passo do atendimento. Agradeço por cuidarem tão bem do meu pequeno. Agora, sinto que ele está saudável e feliz.”' />
+            </section>
         </div>
     )
 }
--- src/components/Feedback/FeedbackContainer/FeedbackContainer.jsx
+++ src/components/Feedback/FeedbackContainer/FeedbackContainer.jsx
@@ -1,10 +1,20 @@
 import './FeedbackContainer.css'
 
-function FeedbackContainer(){
+function FeedbackContainer(props){
     return(
         <div>
+            <section className="feedback-container">
+                <div className="feedback-user-profile">
+                    <img src={props.icon} alt="imagem de perfil" />
+                    <div className="feedback-user-informations">
+                        <h1 className="feedback-user-name">{props.name}</h1>
+                        <p className='feedback-user-date'>{props.date}</p>
+                    </div>
+                </div>
+                <h1 className="feedback-user-text">{props.text}</h1>
+            </section>
         </div>
     )
 }
 
-export default FeedbackContainer();
+export default FeedbackContainer;
